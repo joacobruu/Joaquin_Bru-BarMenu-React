cd929342a95a24c18d0ac35abe17ed7f97f9d52f
Joaquin Bru <brujoaco@gmail.com>
2021-11-30 23:42:10 -0300

    logica carrito y firestore

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,60 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@material-ui/core'
 import { useState } from 'react'
+import { getFirestore } from '../storage/getFirestore'
 
 const Form = ({ cart, total }) => {
   const [open, setOpen] = useState(false)
-
-  console.log(cart);
+  const [nombre, setNombre] = useState('')
+  const [apellido, setApellido] = useState('')
+  const [telefono, setTelefono] = useState('')
+  const [email, setEmail] = useState('')
+  const [checkEmail, setCheckEmail] = useState('')
+  const [direccion, setDireccion] = useState('')
 
   const handleOpen = () => {
     setOpen(true)
   }
 
+
+  const validateEmail = () => {
+    if (email != checkEmail) return alert('El Email debe coincidir!')
+  }
+
   const handleClose = () => {
     setOpen(false)
+  }
+
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    validateEmail()
+    let pedido = {}
+
+    let userInfo = {
+      nombre: nombre,
+      apellido: apellido,
+      telefono: telefono,
+      email: email,
+      direccion: direccion
+    }
+
+    pedido.buyer = userInfo
+    pedido.total = total()
+    pedido.items = cart.map(item => {
+      const id = item.producto.id;
+      const nombre = item.producto.nombre;
+      const precio = item.producto.precio * item.cantidad;
+
+      return {id, nombre, precio}
+    })
+
+    console.log(pedido);
+    
+
+    const db = getFirestore()
+    db.collection('orders').add(pedido)
+    .then(res => console.log(res))
   }
 
   return (
@@ -34,6 +77,7 @@ const Form = ({ cart, total }) => {
           id="nombre"
           label="Nombre"
           type="text"
+          onChange={(e) => {setNombre(e.target.value)}}
           fullWidth
         />
         <TextField
@@ -41,6 +85,15 @@ const Form = ({ cart, total }) => {
           id="apellido"
           label="Apellido"
           type="text"
+          onChange={(e) => {setApellido(e.target.value)}}
+          fullWidth
+        />
+        <TextField
+          margin="dense"
+          id="telefono"
+          label="Telefono"
+          type="number"
+          onChange={(e) => {setTelefono(e.target.value)}}
           fullWidth
         />
         <TextField
@@ -48,6 +101,7 @@ const Form = ({ cart, total }) => {
           id="email"
           label="Email"
           type="email"
+          onChange={(e) => {setEmail(e.target.value)}}
           fullWidth
         />
         <TextField
@@ -55,6 +109,7 @@ const Form = ({ cart, total }) => {
           id="emailConfirm"
           label="Reperir Email"
           type="email"
+          onChange={(e) => {setCheckEmail(e.target.value)}}
           fullWidth
         />
         <TextField
@@ -62,6 +117,7 @@ const Form = ({ cart, total }) => {
           id="direccion"
           label="Direccion"
           type="text"
+          onChange={(e) => {setDireccion(e.target.value)}}
           fullWidth
         />
       </DialogContent>
@@ -69,7 +125,7 @@ const Form = ({ cart, total }) => {
         <Button onClick={handleClose} color="primary">
           Cancelar
         </Button>
-        <Button variant='contained' onClick={handleClose} color="primary">
+        <Button onClick={handleSubmit} variant='contained' color="primary">
           Comprar
         </Button>
       </DialogActions>
